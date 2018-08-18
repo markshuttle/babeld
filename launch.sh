320edb06441da77fc1c23b7b6e602b1495a909a5
Mark Shuttleworth <mark@ubuntu.com>
2018-08-18 21:11:22 +0200

    Initialize with dummy config if not found

--- launch.sh
+++ launch.sh
@@ -3,8 +3,22 @@
 if [ -e $SNAP_COMMON/babeld.pid ]; then
     if pgrep -F $SNAP_COMMON/babeld.pid; then
         echo "Babeld already running"
+        exit 1
     else
         rm $SNAP_COMMON/babeld.pid
     fi
+fi
+
+if [ ! -e $SNAP_COMMON/babeld.conf ]; then
+    /bin/cat <<EOM >$SNAP_COMMON/babeld.conf
+# For more information about this configuration file, refer to
+# https://www.irif.fr/~jch/software/babel/babeld.html
+log-file /var/snap/babeld-sabdfl/common/babeld.log
+state-file /var/snap/babeld-sabdfl/common/babeld.state
+link-detect true
+reflect-kernel-metric true
+interface eth0 type wired rxcost 100
+redistribute local deny
+EOM
 fi
 exec babeld -c $SNAP_COMMON/babeld.conf
